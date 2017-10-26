572cda94580feebfb819e9602a67046ea97b5976
Matt Gruskin <matthew.gruskin@gmail.com>
2017-10-26 05:26:49 +0000

    codes tab UI improvements

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,22 +1,28 @@
 <template>
-  <v-app id="solvent">
-    <v-tabs>
+  <v-app id="solvent" class="full-height">
+    <v-tabs class="full-height">
       <v-tabs-bar class="blue-grey lighten-4">
-        <v-tabs-item href="#codes">Codes</v-tabs-item>
-        <v-tabs-item href="#ciphers">Ciphers</v-tabs-item>
+        <v-tabs-item href="#codes-tab">Codes</v-tabs-item>
+        <v-tabs-item href="#ciphers-tab">Ciphers</v-tabs-item>
         <v-tabs-slider></v-tabs-slider>
       </v-tabs-bar>
-      <v-tabs-items>
-        <v-tabs-content id="codes">
+      <v-tabs-items class="full-height">
+        <v-tabs-content id="codes-tab" class="full-height" lazy>
           <codes-tab-component />
         </v-tabs-content>
-        <v-tabs-content id="ciphers">
+        <v-tabs-content id="ciphers-tab" class="full-height" lazy>
           <ciphers-tab-component />
         </v-tabs-content>
       </v-tabs-items>
     </v-tabs>
   </v-app>
 </template>
+
+<style>
+.full-height {
+  height: 100%;
+}
+</style>
 
 <script lang="ts">
 import Vue from "vue";
--- src/components/CodesTab.vue
+++ src/components/CodesTab.vue
@@ -1,30 +1,75 @@
 <template>
-  <v-container fluid>
+  <v-container fluid id="codes-tab-container">
     <v-layout row>
       <v-flex>
-        <v-text-field label="Text" multi-line v-model="text">
+        <v-text-field id="text" label="Text" textarea rows="3" v-model="text">
         </v-text-field>
       </v-flex>
     </v-layout>
-    <v-layout row>
-      <v-btn color="error" @click="clear">Clear</v-btn>
-      <v-btn color="error" @click="backspace">Backspace</v-btn>
-      <v-btn color="primary" @click="addLetter(' ')">Space</v-btn>
-      <v-btn color="primary" @click="copy">Copy</v-btn>
+
+    <v-layout row wrap align-center>
+      <v-tooltip top>
+        <v-btn slot="activator" fab small color="error" @click="clear">
+          <v-icon>delete_forever</v-icon>
+        </v-btn>
+        <span>Clear text</span>
+      </v-tooltip>
+
+      <v-tooltip top>
+        <v-btn slot="activator" fab small color="error" @click="backspace">
+          <v-icon>backspace</v-icon>
+        </v-btn>
+        <span>Backspace</span>
+      </v-tooltip>
+
+      <v-tooltip top>
+        <v-btn slot="activator" fab small color="primary" @click="addLetter(' ')">
+          <v-icon>forward</v-icon>
+        </v-btn>
+        <span>Insert space</span>
+      </v-tooltip>
+
+      <v-tooltip top>
+        <v-btn slot="activator" fab small color="primary" @click="copy">
+          <v-icon>content_copy</v-icon>
+        </v-btn>
+        <span>Copy to clipboard</span>
+      </v-tooltip>
     </v-layout>
+
     <v-layout row>
       <v-flex>
-        <v-container fluid grid-list-md>
+        <v-container class="hidden-sm-and-down" fluid grid-list-md>
           <v-layout row wrap>
             <v-card>
               <braille-picker-component @addLetter="addLetter($event)" />
             </v-card>
           </v-layout>
         </v-container>
+        <v-tabs class="hidden-md-and-up">
+          <v-tabs-bar class="blue-grey lighten-4">
+            <v-tabs-item href="#braille-tab">Braille</v-tabs-item>
+            <v-tabs-slider></v-tabs-slider>
+          </v-tabs-bar>
+          <v-tabs-items>
+            <v-tabs-content id="braille-tab" lazy>
+              <braille-picker-component @addLetter="addLetter($event)" />
+            </v-tabs-content>
+          </v-tabs-items>
+        </v-tabs>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
+
+<style>
+#codes-tab-container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+</style>
 
 <script lang="ts">
 import Vue from "vue";
@@ -53,6 +98,9 @@ export default class CodesTab extends Vue {
   }
 
   copy() {
+    const textArea: HTMLTextAreaElement = <HTMLTextAreaElement>document.querySelector("#text");
+    textArea.select();
+    document.execCommand("copy");
   }
 }
 </script>
