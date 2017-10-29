77f2a3284e470b3f48a20c08ab35d856f5723b21
Matt Gruskin <matthew.gruskin@gmail.com>
2017-10-29 06:01:15 +0000

    caesar shift calls server for english scores

--- src/components/CaesarShifter.vue
+++ src/components/CaesarShifter.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="caesar-shifter">
     <ul>
-      <li v-for="(s, index) in outputStrings" :key="index">
-        {{ s }}
+      <li
+        v-for="(s, index) in outputStrings"
+        :key="index"
+        :class="{ 'highlight': index === bestOutputStringIndex }"
+      >
+        <span class="shiftAmount">{{ index }}</span>{{ s }}
       </li>
     </ul>
   </div>
@@ -13,11 +17,20 @@
   list-style-type: none;
   padding-left: 0;
 }
+#caesar-shifter ul li.highlight {
+  font-weight: bold;
+}
+.shiftAmount {
+  display: inline-block;
+  min-width: 24px;
+}
 </style>
 
 <script lang="ts">
 import Vue from "vue";
 import { Component, Prop } from "vue-property-decorator";
+
+import { SCORE_ENGLISH_URL, ScoreEnglishRequest, ScoreEnglishResponse } from "../api/score_english";
 
 @Component
 export default class CaesarShifter extends Vue {
@@ -25,10 +38,12 @@ export default class CaesarShifter extends Vue {
   public inputString: string;
 
   private outputStrings: string[] = [];
+  private bestOutputStringIndex: number = -1;
 
   public update() {
     const strippedInputString = this.inputString.toUpperCase().replace(/[^A-Z ]/g, "");
     this.outputStrings = [];
+    this.bestOutputStringIndex = -1;
     for (let shift = 0; shift < 26; shift++) {
       let outputString = "";
       for (let i = 0; i < strippedInputString.length; i++) {
@@ -42,6 +57,22 @@ export default class CaesarShifter extends Vue {
       }
       this.outputStrings.push(outputString);
     }
+    this.getScores();
+  }
+
+  private getScores() {
+    const request: ScoreEnglishRequest = {
+      texts: this.outputStrings,
+    };
+    fetch(SCORE_ENGLISH_URL, {
+      body: JSON.stringify(request),
+      headers: new Headers({ "Content-Type": "application/json" }),
+      method: "POST",
+    }).then((response) => response.json())
+    .then((response: ScoreEnglishResponse) => {
+      const maxScore = Math.max(...response.scores);
+      this.bestOutputStringIndex = response.scores.indexOf(maxScore);
+    });
   }
 }
 </script>
