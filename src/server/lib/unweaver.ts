3cde11b24d3f25659af7f4aff54f48971ca22265
Matt Gruskin <matthew.gruskin@gmail.com>
2017-11-12 05:09:34 +0000

    some unweaver optimizations

--- src/server/lib/unweaver.ts
+++ src/server/lib/unweaver.ts
@@ -17,10 +17,15 @@ class Letter {
   }
 }
 
-type Word = Letter[];
+class Word {
+  constructor(
+    public letters: Letter[] = [],
+    public cost: number | null = null) {
+  }
+}
 
 function wordString(word: Word) {
-  return _.map(word, "letter").join("");
+  return _.map(word.letters, "letter").join("");
 }
 
 class Step {
@@ -39,12 +44,13 @@ export function unweave(text: string): Promise<Result[]> {
     for (let i = 0; i < text.length; i++) {
       initialLetters.push(new Letter(i, text[i]));
     }
-    const initialSolution = initialLetters.map((letter) => [letter]);
+    const initialSolution = initialLetters.map(
+      (letter) => new Word([letter], -fitnessStats.wordScore(letter.letter)));
     const solver = new SolutionCostSolver<Word[]>(
       initialSolution,
-      (words) => -fitnessStats.wordListScore(words.map(wordString)));
+      (words) => _.sumBy(words, "cost"));
 
-    const stepQueue: Step[] = [new Step([], fitnessStats.wordLogProbsTrie, initialLetters, 0, [])];
+    const stepQueue: Step[] = [new Step(new Word(), fitnessStats.wordLogProbsTrie, initialLetters, 0, [])];
     function runStep() {
       if (stepQueue.length === 0) {
         resolve(solver.getResults().map((result) => new Result(
@@ -52,23 +58,44 @@ export function unweave(text: string): Promise<Result[]> {
           result.cost)));
         return;
       }
+
       const step = stepQueue.pop() as Step;
-      if (step.prefix.length === 0 && step.letters.length === 0) {
-        solver.addResult(_.sortBy(step.words, (word) => word[0].position));
+
+      const wordsCost = _.sumBy(step.words, "cost");
+      const results = solver.getResults();
+      if (wordsCost > results[results.length - 1].cost) {
+        setImmediate(runStep);
+        return;
       }
-      for (let lettersIndex = step.lettersStart; lettersIndex < step.letters.length; lettersIndex++) {
+
+      let lettersIndexLimit = step.letters.length;
+      if (step.prefix.letters.length === 0) {
+        if (step.letters.length === 0) {
+          const solution = _.sortBy(step.words, (word) => word.letters[0].position);
+          solver.addResult(solution);
+        } else {
+          lettersIndexLimit = step.lettersStart + 1;
+        }
+      }
+
+      for (let lettersIndex = step.lettersStart; lettersIndex < lettersIndexLimit; lettersIndex++) {
         const letter = step.letters[lettersIndex];
         const childNode = step.prefixTrieNode.children[letter.letter];
         if (childNode !== undefined) {
-          const word = step.prefix.concat([letter]);
+          const word = new Word(step.prefix.letters.concat([letter]));
           const nextLetters = _.clone(step.letters);
           nextLetters.splice(lettersIndex, 1);
           if (childNode.value !== null) {
-            stepQueue.push(new Step([], fitnessStats.wordLogProbsTrie, nextLetters, 0, step.words.concat([word])));
+            word.cost = -childNode.value;
+            const nextWords = step.words.concat([word]);
+            const nextWordStep = new Step(new Word(), fitnessStats.wordLogProbsTrie, nextLetters, 0, nextWords);
+            stepQueue.push(nextWordStep);
           }
-          stepQueue.push(new Step(word, childNode, nextLetters, lettersIndex, step.words));
+          const nextStep = new Step(word, childNode, nextLetters, lettersIndex, step.words);
+          stepQueue.push(nextStep);
         }
       }
+
       setImmediate(runStep);
     }
     setImmediate(runStep);
