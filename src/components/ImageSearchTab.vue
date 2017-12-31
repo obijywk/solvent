ad13d7a1af33671b575ff072a7760a255e370541
Matt Gruskin <matthew.gruskin@gmail.com>
2017-12-31 14:50:22 +0000

    start setting up API for google cloud vision web detection

--- src/components/ImageSearchTab.vue
+++ src/components/ImageSearchTab.vue
@@ -7,8 +7,22 @@
     <div id="image-search-image-container">
       <img ref="image" />
     </div>
-    <div>
-      <img ref="croppedImage" />
+    <div id="image-search-results">
+      <div id="image-search-progress" v-if="state === State.RUNNING">
+        <v-progress-circular indeterminate v-bind:size="64" color="primary" />
+      </div>
+      <v-data-table
+        :headers="headers"
+        :items="results"
+        hide-actions
+        id="image-search-tab-table"
+        v-if="state === State.RESULTS"
+      >
+        <template slot="items" slot-scope="props">
+          <td>{{ props.item.score }}</td>
+          <td>{{ props.item.description }}</td>
+        </template>
+      </v-data-table>
     </div>
   </div>
 </template>
@@ -34,9 +48,31 @@ import { Component } from "vue-property-decorator";
 
 import * as Cropper from "cropperjs";
 
+import {
+  RUN_WEB_DETECTION_URL,
+  RunWebDetectionRequest,
+  RunWebDetectionResponse,
+  RunWebDetectionResult,
+} from "../api/run_web_detection";
+import { apiFetch } from "../client/api_fetch";
+
+enum State {
+  RUNNING,
+  RESULTS,
+}
+
 @Component
 export default class ImageSearchTab extends Vue {
   private cropper: Cropper | null = null;
+  private results: RunWebDetectionResult[] = [];
+
+  private State = State;
+  private state: State = State.RESULTS;
+
+  private headers = [
+    { text: "Score", value: "score", align: "left", sortable: false },
+    { text: "Description", value: "description", align: "left", sortable: false },
+  ];
 
   private handleFileInputChange() {
     const fileInput = this.$refs.fileInput as HTMLInputElement;
@@ -65,7 +101,20 @@ export default class ImageSearchTab extends Vue {
     }
     const canvas = this.cropper.getCroppedCanvas();
     const croppedImage = this.$refs.croppedImage as HTMLImageElement;
-    croppedImage.src = canvas.toDataURL();
+    const dataUrl = canvas.toDataURL();
+
+    const request: RunWebDetectionRequest = {
+      imageContent: dataUrl,
+    };
+    this.state = State.RUNNING;
+    apiFetch<RunWebDetectionResponse>(RUN_WEB_DETECTION_URL, request).then((response) => {
+      this.results = response.results;
+      this.state = State.RESULTS;
+    }).catch((err: Error) => {
+      this.results = [];
+      this.state = State.RESULTS;
+      this.$emit("error", err.message);
+    });
   }
 }
 </script>
